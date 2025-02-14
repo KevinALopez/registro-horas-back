9bc9c23899031b2b4faa0631de53632d699e622a
KevinALopez <005.kevin.ayala@gmail.com>
2025-02-14 17:40:16 +0100

    Implement a generateToken function and the loginUser function for auth.model and auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,44 @@
-const { searchUserByUsername } = require("../models/auth.model");
+const bcrypt = require("bcryptjs");
+const { searchUserByUsername, generateToken } = require("../models/auth.model");
 
+/**
+ * Authenticates a user and generates a JWT token if credentials are valid.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} req.body - The request body containing login credentials.
+ * @param {string} req.body.username - The username of the user.
+ * @param {string} req.body.password - The password of the user.
+ * @param {Object} res - The response object used to send back the result.
+ * @param {Function} next - The next middleware function to handle errors.
+ * @returns {Promise<void>} Sends a JSON response with a success message and JWT token if login is successful,
+ * or an error message if authentication fails.
+ */
 const loginUser = async (req, res, next) => {
     const { username, password } = req.body;
 
     try {
-        const user = await searchUserByUsername(username);
+        const result = await searchUserByUsername(username);
 
-        res.json(result);
+        if (result.length === 0) {
+            return res
+                .status(401)
+                .json({ message: "Wrong username or password." });
+        }
+
+        const passwordMatch = bcrypt.compareSync(password, result[0].password);
+
+        if (!passwordMatch) {
+            return res
+                .status(401)
+                .json({ message: "Wrong username or password." });
+        }
+
+        const token = generateToken(result[0]);
+
+        res.json({
+            message: "Login successful.",
+            token,
+        });
     } catch (error) {
         next(error);
     }
--- src/models/auth.model.js
+++ src/models/auth.model.js
@@ -1,20 +1,38 @@
 const db = require("../config/db.js");
-const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+/**
+ * Searches for a user in the database by their username.
+ *
+ * @param {string} username - The username of the user to search for.
+ * @returns {Promise<Array>} A promise that resolves to an array containing the user data if found, or an empty array if no user is found.
+ */
 const searchUserByUsername = async (username) => {
-    try {
-        const [result] = await db.query(
-            "SELECT * FROM users WHERE username = ?",
-            [username]
-        );
+    const [result] = await db.query("SELECT * FROM users WHERE username = ?", [
+        username,
+    ]);
 
-        return result;
-    } catch (error) {
-        console.log(error);
-        return [];
-    }
+    return result;
+};
+
+/**
+ * Generates a JSON Web Token (JWT) for a user.
+ *
+ * @param {Object} user - The user object containing user details.
+ * @param {number} user.id - The unique ID of the user.
+ * @param {string} user.role - The role of the user.
+ * @returns {string} A signed JWT token.
+ */
+const generateToken = (user) => {
+    const token = jwt.sign(
+        { id: user.id, role: user.role },
+        process.env.JWT_SECRET
+    );
+
+    return token;
 };
 
 module.exports = {
     searchUserByUsername,
+    generateToken,
 };
