d7526afc7e38c6e76065800d9f5217e81b7e7d73
KevinALopez <005.kevin.ayala@gmail.com>
2025-02-14 17:06:06 +0100

    Implement searchUserByUsername function on auth.model.js file

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,15 @@
-const loginUser = (req, res, next) => {
-    console.log("TODO");
+const { searchUserByUsername } = require("../models/auth.model");
+
+const loginUser = async (req, res, next) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await searchUserByUsername(username);
+
+        res.json(result);
+    } catch (error) {
+        next(error);
+    }
 };
 
 module.exports = {
--- src/models/auth.model.js
+++ src/models/auth.model.js
@@ -1,5 +1,20 @@
-const searchUserByEmailAndPassword = async (email, password) => {};
+const db = require("../config/db.js");
+const bcrypt = require("bcryptjs");
+
+const searchUserByUsername = async (username) => {
+    try {
+        const [result] = await db.query(
+            "SELECT * FROM users WHERE username = ?",
+            [username]
+        );
+
+        return result;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+};
 
 module.exports = {
-    searchUserByEmailAndPassword,
+    searchUserByUsername,
 };
