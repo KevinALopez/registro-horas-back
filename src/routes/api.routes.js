07c968570ef92078dc53eb6adad4c4d5111e1d9f
KevinALopez <005.kevin.ayala@gmail.com>
2025-02-16 10:49:39 +0100

    add /api/hours route

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -4,6 +4,7 @@ const router = require("express").Router();
 
 router.use("/auth", require("./api/auth.routes.js"));
 router.use("/projects", require("./api/projects.routes.js"));
-router.use("/users", require("./api/users.routes.js"))
+router.use("/users", require("./api/users.routes.js"));
+router.use("/hours", require("./api/hours.routes.js"));
 
 module.exports = router;
---
+++ src/routes/api/hours.routes.js
@@ -0,0 +1,5 @@
+// api/hours
+
+const router = require("express").Router();
+
+module.exports = router;
