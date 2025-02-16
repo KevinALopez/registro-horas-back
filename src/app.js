886efc652f0f1178c1a79aeb33900df1d0fb4ab3
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-02-16 15:57:28 +0100

    agregue  app.use(express.urlencoded({ extended: true })); para que funcione mejor

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", require("./routes/api.routes.js"));
 
