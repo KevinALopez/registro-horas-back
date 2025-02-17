49ccd60d43f5d4260868de9f795aa6176914f85b
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-02-17 12:14:50 +0100

    correction de error en merge

--- src/routes/api/hours.routes.js
+++ src/routes/api/hours.routes.js
@@ -1,5 +1,3 @@
-const express = require("express");
-const { getAllHoursByMonth, getHoursWorkedByDate } = require("../../controllers/hours.controller");
 
 const router = require("express").Router();
 
@@ -16,6 +14,6 @@ router.get("/:month", checkToken, getAllHoursByMonth);
 router.post("/start", checkToken, registerWorkdayStart);
 router.post("/end", checkToken, registerWorkdayEnd);
 
-router.post('/', getHoursWorkedByDate);
+router.post('/', checkToken, getHoursWorkedByDate);
 
 module.exports = router;
