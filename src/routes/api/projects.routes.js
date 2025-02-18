2dab7be0e707fa94762d358bc3f1f5c5f3b54541
KevinALopez <005.kevin.ayala@gmail.com>
2025-02-18 10:37:54 +0100

    fixec bug in getProjectById controller

--- src/routes/api/projects.routes.js
+++ src/routes/api/projects.routes.js
@@ -2,17 +2,17 @@ const {
     deleteProjectById,
     updateProjectById,
     getAllProjects,
-
+    getProjectById,
 } = require("../../controllers/projects.controller");
 const { checkToken, checkAdmin } = require("../../middlewares/auth.middleware");
 const { createNewProject, selectById } = require("../../models/projects.model");
 
 const router = require("express").Router();
 
-router.get("/:id", selectById);
+router.get("/:id", getProjectById);
 router.delete("/:id", checkToken, deleteProjectById);
 router.put("/:id", checkToken, checkAdmin, updateProjectById);
 router.get("", checkToken, getAllProjects);
-router.post("", checkToken, checkAdmin, createNewProject)
+router.post("", checkToken, checkAdmin, createNewProject);
 
 module.exports = router;
