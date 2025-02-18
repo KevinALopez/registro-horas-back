5c8a8ff0d20914c23adbfb82675d0f5b0e537347
marlon2219 <marloncajamarca@gmail.com>
2025-02-18 10:43:51 +0100

    implementancion de añadir star en pause

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -7,4 +7,5 @@ router.use("/projects", require("./api/projects.routes.js"));
 router.use("/users", require("./api/users.routes.js"));
 router.use("/hours", require("./api/hours.routes.js"));
 
+
 module.exports = router;
--- src/routes/api/hours.routes.js
+++ src/routes/api/hours.routes.js
@@ -7,12 +7,17 @@ const {
     registerWorkdayEnd,
     getHoursWorkedByDate,
 } = require("../../controllers/hours.controller");
+const {
+    insertPause,
+} = require("../../controllers/pauses.controller");
 
 const { checkToken } = require("../../middlewares/auth.middleware");
 
 router.get("/:month", checkToken, getAllHoursByMonth);
 router.post("/start", checkToken, registerWorkdayStart);
 router.post("/end", checkToken, registerWorkdayEnd);
+
+router.post("/pause/start",checkToken, insertPause);
 
 router.post("/", getHoursWorkedByDate);
 router.post('/', checkToken, getHoursWorkedByDate);
